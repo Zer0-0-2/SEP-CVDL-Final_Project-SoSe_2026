63f6cfccb4f22cec5f4ff7659a618362d49cdd7f
Vally-GO <75756235+Vally-GO@users.noreply.github.com>
2026-06-29 02:07:27 +0200

    updated readme for AMD systems and reverted comment on inference.py

---
+++ animal-recognition/src/data/reddit_downloader.py
@@ -0,0 +1,4 @@
+# original command
+# gallery-dl --cookies-from-browser firefox --abort 200 -d "~/Downloads/rips/golden_retriever" "https://www.reddit.com/r/goldenretrievers/top/?screen_view_count=2&t=all"
+
+#

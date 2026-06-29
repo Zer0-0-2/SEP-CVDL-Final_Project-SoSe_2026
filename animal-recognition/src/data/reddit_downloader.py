4c0a142bdb020bf36b3f6548310c1ebbed2340ae
Vally-GO <75756235+Vally-GO@users.noreply.github.com>
2026-06-29 02:32:41 +0200

    prepared reddit downloader

--- animal-recognition/src/data/reddit_downloader.py
+++ animal-recognition/src/data/reddit_downloader.py
@@ -2,3 +2,65 @@
 # gallery-dl --cookies-from-browser firefox --abort 200 -d "~/Downloads/rips/golden_retriever" "https://www.reddit.com/r/goldenretrievers/top/?screen_view_count=2&t=all"
 
 #
+#
+#   CLASSES = [
+#       "Abyssinian",         #  0
+#       "Bengal",             #  1
+#       "Birman",             #  2
+#       "Bombay",             #  3
+#       "British_Shorthair",  #  4
+#       "Maine_Coon",         #  5
+#       "Ragdoll",            #  6
+#       "Sphynx",             #  7
+#       "Tabby",              #  8
+#       "Tiger_Cat",          #  9
+#       "Beagle",             # 10
+#       "Pug",                # 11
+#       "Boxer",              # 12
+#       "Shiba_Inu",          # 13
+#       "Samoyed",            # 14
+#       "Golden_Retriever",   # 15
+#       "German_Shepherd",    # 16
+#       "Siberian_Husky",     # 17
+#       "Dalmatian",          # 18
+#       "Rottweiler",         # 19
+#   ]
+
+
+#    https://www.reddit.com/r/Abyssinians/  
+#    https://www.reddit.clom/r/bengalcats/ 
+#    https://www.reddit.com/r/birmans/ 
+#    https://www.reddit.com/r/BombayCat/
+#    https://www.reddit.com/r/britishshorthair/ 
+#    https://www.reddit.com/r/mainecoons/ 
+#    https://www.reddit.com/r/ragdollcats/ 
+#    https://www.reddit.com/r/SphynxCats/ 
+#    https://www.reddit.com/r/TabbyCats/
+#    TODO: find some other source
+#    https://www.reddit.com/r/beagles/
+#    https://www.reddit.com/r/pugs/
+#    https://www.reddit.com/r/Boxer/
+#    https://www.reddit.com/r/shiba/
+#    https://www.reddit.com/r/samoyeds/
+#    https://www.reddit.com/r/goldenretrievers/ 
+#    https://www.reddit.com/r/germanshepherds/
+#    https://www.reddit.com/r/siberianhusky/
+#    https://www.reddit.com/r/dalmatians/
+#    https://www.reddit.com/r/Rottweiler/
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+   
+   
+   
+   
+   

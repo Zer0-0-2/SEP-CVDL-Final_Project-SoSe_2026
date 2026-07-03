a4a0e77eae8db32d57f85a0c5b1b676ad73954a6
Vally-GO <75756235+Vally-GO@users.noreply.github.com>
2026-07-03 20:28:27 +0200

    added rejection folder for easier debugging

--- animal_recognition/src/models/yolo26.py
+++ animal_recognition/src/models/yolo26.py
@@ -41,82 +41,84 @@ DOG_BREEDS = {
 # it takes the biggest bounding box of a cat or dog depending on the species (thus images with a dog and a cat in for exmaple) 
 # r/abyssinian will just return the cat since we presume that's the image we care about for training.
 # this is probably not perfect but it should work for now. Make sure that your animal_recognition/data/processed is empty before use
-def process_dataset(raw_dir: str, processed_dir: str, model_name: str = 'yolo26n.pt', debug: bool = False):
+def process_dataset(raw_dir: Path, processed_dir: Path, rejected_dir: Path, model_name: str = 'yolo26n.pt', debug: bool = False):
     model = YOLO(model = animal_recog_dir / 'models' / model_name, verbose = False)
-    raw_path = Path(raw_dir)
-    processed_path = Path(processed_dir)
-    
     # debug 
     if debug:
-        print(raw_path) 
-        print(processed_path)
+        print(raw_dir) 
+        print(processed_dir)
+        print(rejected_dir)
     
-    if not debug:
-        for breed in os.listdir(raw_path):
-            breed_dir = raw_path / breed
+    for breed in os.listdir(raw_dir):
+        breed_dir = raw_dir / breed
+        
+        if not breed_dir.is_dir():
+            print("error, not a directory")
+            exit(1)
             
-            if not breed_dir.is_dir():
-                print("error, not a directory")
-                exit(1)
+        if breed in CAT_BREEDS:
+            target_class = 15  
+        elif breed in DOG_BREEDS:
+            target_class = 16
+        else:
+            print(f"error, please download the correct dataset from filen and unpack it correctly -_- for breed: {breed}")
+            exit(1)
+            
+            
+        out_breed_dir = processed_dir / breed
+        out_breed_dir.mkdir(parents=True, exist_ok=True)
+        
+        out_rejected_breed_dir = rejected_dir / breed
+        out_rejected_breed_dir.mkdir(parents=True, exist_ok=True)
+        
+        print(f"Processing {breed}")
+        
+        for img_name in os.listdir(breed_dir):
                 
-            if breed in CAT_BREEDS:
-                target_class = 15  
-            elif breed in DOG_BREEDS:
-                target_class = 16
+            img_path = breed_dir / img_name
+            img = cv2.imread(str(img_path))
+            if img is None:
+                print(f"Failed to load image: {img_path}")
+                continue
+                
+            results = model(img, verbose=False) # returns a list as result, not single item, verbose = false to make console less cluttered
+            
+            largest_area = 0
+            best_box = None
+            
+            
+            # https://docs.ultralytics.com/tasks/detect#results-output 
+            for result in results:
+                for box in result.boxes:
+                    cls_id = int(box.cls[0].item())
+                    if cls_id == target_class:
+                        x1, y1, x2, y2 = box.xyxy[0].tolist()
+                        confidence = box.conf[0].item()
+                        area = (x2 - x1) * (y2 - y1)
+                        if area > largest_area:
+                            largest_area = area
+                            best_box = (int(x1), int(y1), int(x2), int(y2))
+                            best_box_confidence = confidence
+                            img_name = str(best_box_confidence) + "_" + img_name 
+            
+            if best_box is not None:
+                x1, y1, x2, y2 = best_box
+                cropped = img[y1:y2, x1:x2]
             else:
-                print(f"error, please download the correct dataset from filen and unpack it correctly -_- for breed: {breed}")
-                exit(1)
+                cv2.imwrite(str(out_rejected_breed_dir / img_name), img) 
+                print(f"No cat or dog found in image: {img_path}, saving to rejected folder") 
+                continue # In case no dog or cat is found in a dog or cat breed folder
+                            # which should happen pretty rarely, but i remember seeing a couple of images
+                            # while scraping that had no dog or cat in them (example one person with a dog bite)
                 
-                
-            out_breed_dir = processed_path / breed
-            out_breed_dir.mkdir(parents=True, exist_ok=True)
-            
-            print(f"Processing {breed}")
-            
-            for img_name in os.listdir(breed_dir):
-                    
-                img_path = breed_dir / img_name
-                img = cv2.imread(str(img_path))
-                if img is None:
-                    print(f"Failed to load image: {img_path}")
-                    continue
-                    
-                results = model(img, verbose=False) # returns a list as result, not single item, verbose = false to make console less cluttered
-                
-                largest_area = 0
-                best_box = None
-                
-                
-                # https://docs.ultralytics.com/tasks/detect#results-output 
-                for result in results:
-                    for box in result.boxes:
-                        cls_id = int(box.cls[0].item())
-                        if cls_id == target_class:
-                            x1, y1, x2, y2 = box.xyxy[0].tolist()
-                            confidence = box.conf[0].item()
-                            area = (x2 - x1) * (y2 - y1)
-                            if area > largest_area:
-                                largest_area = area
-                                best_box = (int(x1), int(y1), int(x2), int(y2))
-                                best_box_confidence = confidence
-                                img_name = str(best_box_confidence) + "_" + img_name 
-                
-                if best_box is not None:
-                    x1, y1, x2, y2 = best_box
-                    cropped = img[y1:y2, x1:x2]
-                else:
-                    print(f"No cat or dog found in image: {img_path}") 
-                    continue # skip in case no dog or cat is found in a dog or cat breed folder
-                             # which should happen pretty rarely, but i remember seeing a couple of images
-                             # while scraping that had no dog or cat in them (example one person with a dog bite)
-                    
-                out_path = out_breed_dir / img_name
-                cv2.imwrite(str(out_path), cropped)
+            out_path = out_breed_dir / img_name
+            cv2.imwrite(str(out_path), cropped)
 
 if __name__ == '__main__':
     current_dir = Path(__file__).resolve().parent
     animal_recog_dir = current_dir.parent.parent
     raw_dir = animal_recog_dir / 'data' / 'raw'
     processed_dir = animal_recog_dir / 'data' / 'processed'
+    rejected_dir = animal_recog_dir / 'data' / 'rejected'
     
-    process_dataset(str(raw_dir), str(processed_dir), debug=False)
+    process_dataset(raw_dir, processed_dir, rejected_dir, debug=False)

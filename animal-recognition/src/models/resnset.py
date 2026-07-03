bad6d6ff9651f9aac897d84313d65f5bddbec726
Vally-GO <75756235+Vally-GO@users.noreply.github.com>
2026-07-03 17:42:15 +0200

    merged from main

---
+++ animal-recognition/src/models/resnset.py
@@ -0,0 +1,4 @@
+# this is my implemenation of a resnet50
+# references: https://lekhuyen.medium.com/a-quick-overview-of-resnet-models-f8ed277ae81e
+
+# TODO: Warning: this implementation is not complete yet

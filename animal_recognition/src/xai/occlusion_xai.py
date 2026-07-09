43a492ef826ea6d4e175d7f48c4330321b78518f
Tristan Thenikl <thenikl@cip.ifi.lmu.de>
2026-07-09 15:39:04 +0200

    added occlusion xai methode

---
+++ animal_recognition/src/xai/occlusion_xai.py
@@ -0,0 +1,13 @@
+import argparse
+from pathlib import Path
+
+import cv2
+import numpy as np
+import torch
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from PIL import Image
+from torchvision import transforms
+
+from src.config import load_config
+from src.models.baseline_cnn import BaselineCNN

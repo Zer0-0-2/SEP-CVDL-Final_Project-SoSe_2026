aa622e341bc49570b4f4e232b736e758d907f77b
Vally-GO <75756235+Vally-GO@users.noreply.github.com>
2026-07-09 18:51:37 +0200

    rewritten convnext using timm

--- animal_recognition/src/models/classifier_convnext.py
+++ animal_recognition/src/models/classifier_convnext.py
@@ -1,15 +1,6 @@
 import torch
 import torch.nn as nn
-from torchvision.models import (
-    convnext_tiny,
-    convnext_small,
-    convnext_base,
-    convnext_large,
-    ConvNeXt_Tiny_Weights,
-    ConvNeXt_Small_Weights,
-    ConvNeXt_Base_Weights,
-    ConvNeXt_Large_Weights,
-)
+import timm
 from pathlib import Path
 
 # Import the dataset classes to determine the number of classes
@@ -22,44 +13,12 @@ class ConvNextClassifier(nn.Module):
         Model names: "convnext_tiny", "convnext_small", "convnext_base", "convnext_large"
         the first three have 224x224 input size, the last one has 384x384 input size (probably too large for compute resources)
         """
-
         super().__init__()
         self.num_classes = len(animal_dataset.CLASSES)
 
-        # for later
-        if not pretrained:
-            weights = None
-        else:
-            if model_name == "convnext_tiny":
-                weights = ConvNeXt_Tiny_Weights.DEFAULT
-            elif model_name == "convnext_small":
-                weights = ConvNeXt_Small_Weights.DEFAULT
-            elif model_name == "convnext_base":
-                weights = ConvNeXt_Base_Weights.DEFAULT
-            elif model_name == "convnext_large":
-                weights = ConvNeXt_Large_Weights.DEFAULT
-            else:
-                raise ValueError(f"unknown model name: {model_name}")
-
-        if model_name == "convnext_tiny":
-            model = convnext_tiny(weights=weights)
-        elif model_name == "convnext_small":
-            model = convnext_small(weights=weights)
-        elif model_name == "convnext_base":
-            model = convnext_base(weights=weights)
-        elif model_name == "convnext_large":
-            model = convnext_large(weights=weights)
-        else:
-            raise ValueError(f"unknown model name: {model_name}")
-
-        self.model: nn.Module = model
-
-        # assert needed to fix some IDE issue, runs fine without it as well though
-        last_layer = self.model.classifier[-1]
-        assert isinstance(last_layer, nn.Linear)
-        in_features: int = last_layer.in_features
-
-        self.model.classifier[-1] = nn.Linear(in_features, self.num_classes)
+        self.model = timm.create_model(
+            model_name, pretrained=pretrained, num_classes=self.num_classes
+        )
 
     def forward(self, x: torch.Tensor):
         return self.model(x)

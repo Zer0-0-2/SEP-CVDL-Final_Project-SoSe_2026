40ef881b7a7878d341d213eaa5e5c58521b4898d
Vally-GO <75756235+Vally-GO@users.noreply.github.com>
2026-07-10 17:26:38 +0200

    added quick resnet to train from random weights later for reference

---
+++ animal_recognition/src/models/classifier_resnet50.py
@@ -0,0 +1,39 @@
+import torch
+import torch.nn as nn
+import timm
+from pathlib import Path
+
+# Import the dataset classes to determine the number of classes
+import animal_recognition.src.data.dataset as animal_dataset
+
+
+# https://github.com/NVlabs/GCVit
+class GCViTClassifier(nn.Module):
+    def __init__(self, pretrained: bool = False, model_name: str = "resnet50"):
+        """
+        timm.list_models() output
+        ['gcvit_base', 'gcvit_small', 'gcvit_tiny', 'gcvit_xtiny', 'gcvit_xxtiny']
+        """
+        super().__init__()
+        self.num_classes = len(animal_dataset.CLASSES)
+
+        self.model = timm.create_model(
+            model_name, pretrained=pretrained, num_classes=self.num_classes
+        )
+
+        self.architecture = "resnet"
+
+        self.model_name = model_name
+        self.pretrained = pretrained
+
+    def forward(self, x: torch.Tensor):
+        return self.model(x)
+
+    def predict(self, x: torch.Tensor):
+        with torch.no_grad():
+            self.model.eval()
+            logits = self.model(x)
+            probabilities = torch.softmax(logits, dim=1)
+            confidences, class_indices = torch.max(probabilities, dim=1)
+
+        return confidences, class_indices

703a93b3ee0c737c784f616ca0e458afe04a6771
torstenslbr <tri.the.foegg@gmail.com>
2026-07-11 16:33:43 +0200

    added get last conflayer to convnext classifier

--- animal_recognition/src/models/classifier_convnext.py
+++ animal_recognition/src/models/classifier_convnext.py
@@ -68,3 +68,6 @@ class ConvNextClassifier(nn.Module):
             confidences, class_indices = torch.max(probabilities, dim=1)
 
         return confidences, class_indices
+    
+    def get_last_conv_layer(self) -> nn.Module:
+        return self.model.stages[-1].blocks[-1].conv_dw

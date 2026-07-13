7d6b6f5fa3249b146a442f76a3de9af5123b1e26
Vally-GO <75756235+Vally-GO@users.noreply.github.com>
2026-07-13 18:50:53 +0200

    changed to bitfit tiny and bitfit base, currently training tiny

--- animal_recognition/src/training/train_bitfit_base.py
+++ animal_recognition/src/training/train_bitfit_base.py
@@ -64,10 +64,9 @@ def start_training(model_name, lr, scheduler_type, note, warmup_t):
 
 
 def main():
-    learning_rates = [1e-1, 5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5, 1e-5, 5e-6, 1e-6, 5e-7, 1e-7]
+    learning_rates = [1e-1, 5e-2, 1e-2, 5e-3, 1e-3, 5e-4, 1e-4, 5e-5]
     models = [
         "gcvit_base",
-        "gcvit_tiny",
     ]
 
     for lr in learning_rates:
